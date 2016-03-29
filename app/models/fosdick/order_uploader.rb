d652b0d55c66afafcddfc31b274315ad989f6f17
Manuel Martinez <vmmf08@gmail.com>
2016-03-29 13:07:56 -0600

    Refactor order uploader

--- app/models/fosdick/order_uploader.rb
+++ app/models/fosdick/order_uploader.rb
@@ -1,20 +1,27 @@
 class Fosdick::OrderUploader
-  def upload_orders
-    exporter = Fosdick::OrderExporter.new(orders)
-    uploader = Fosdick::FileUploader.new(
-        host: Fosdick.config.ftp_host,
-        username: Fosdick.config.ftp_username,
-        password: Fosdick.config.ftp_password,
-        upload_path: Fosdick.config.ftp_upload_path
-    )
+  def initialize(orders)
+    @orders = orders
+  end
 
+  def upload
     exporter.export
     uploader.upload(exporter.header_path, exporter.details_path)
   end
 
   private
 
-  def orders
-    Spree::Order.complete.limit(10)
+  attr_reader :orders
+
+  def exporter
+    @exporter ||= Fosdick::OrderExporter.new(orders)
+  end
+
+  def uploader
+    @uploader ||= Fosdick::FileUploader.new(
+      host: Fosdick.config.ftp_host,
+      username: Fosdick.config.ftp_username,
+      password: Fosdick.config.ftp_password,
+      upload_path: Fosdick.config.ftp_upload_path
+    )
   end
 end
